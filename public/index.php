76c453ba283e2dcb798ced546d9e3e9f0295e81a
Byron <bgva2005@yahoo.com>
2017-06-28 22:53:02 -0500

    FIN video 1

--- public/index.php
+++ public/index.php
@@ -1,6 +1,23 @@
 <?php 
 
-
+/*
 echo "prueba hola mundo";
 echo '<br>';
-echo $_GET['url'];
+echo $_GET['url']; //Probar que traiga contr/modl/arg
+*/
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', realpath(dirname(__FILE__)) . DS);
+define('APP_PATH', ROOT . 'application' . DS);
+//echo ROOT; //verificar root
+
+require_once APP_PATH . 'Config.php';
+require_once APP_PATH . 'Request.php';
+require_once APP_PATH . 'Bootstrap.php';
+require_once APP_PATH . 'Controller.php';
+require_once APP_PATH . 'Model.php';
+require_once APP_PATH . 'View.php';
+require_once APP_PATH . 'Register.php';
+
+//echo '<pre>'; print_r(get_required_files()); //verificar correccto importacion de archivos.
+
+
